da434659f963ded3a7c3a3428ff6d0e6a7bfc4ad
caok1 <caok1@tcnj.edu>
2015-04-04 20:41:12 -0400

    first commit

---
+++ app/src/main/java/software/engineering/gringle/CreateActivity.java
@@ -0,0 +1,15 @@
+package software.engineering.gringle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kevin on 4/1/15.
+ */
+public class CreateActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
